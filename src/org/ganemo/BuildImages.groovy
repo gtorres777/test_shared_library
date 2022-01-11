0a7b9a629f6d2809072f90a69666c72cbd8c2eab
gtorres777 <51068028+gtorres777@users.noreply.github.com>
2022-01-10 20:15:40 -0500

    Update BuildImages.groovy

--- src/org/ganemo/BuildImages.groovy
+++ src/org/ganemo/BuildImages.groovy
@@ -1,25 +1,21 @@
 package org.ganemo
 
 class BuildImages implements Serializable {
+  
   def steps
-  BuildImages(steps) {this.steps = steps}
+  
+  BuildImages(steps) {
+    this.steps = steps
+  }
   
   def mvn(args) {
     steps.echo "${args}"
   }
   
-  def aea() {
-    steps.echo "HOLAAA"
+  def aea(args) {
+    steps.echo "HOLAAA ${args}"
   }
   
-  def cloneRepositories() {
-    withCredentials([gitUsernamePassword(credentialsId: 'odoopartnersid',
-                                        gitToolName: 'git-tool')]) {
-      steps.sh "chmod +x scripts/clone_repositories.sh" 
-      steps.sh "./scripts/clone_repositories.sh 15-dev" 
-    }
   
-  }
   
 }
-

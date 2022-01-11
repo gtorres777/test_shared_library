a5a44903a5f481f00ef448efa509a27cc900c208
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-01-11 18:01:46 -0500

    testing7 class library

--- src/org/ganemo/BuildImages.groovy
+++ src/org/ganemo/BuildImages.groovy
@@ -23,8 +23,15 @@ class BuildImages implements Serializable {
             returnStdout: true
             ).replaceAll('\n', ', ')
 
+    steps.withCredentials([steps.usernamePassword(credentialsId: "${config.registryCredential}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        config.existing_tags_dockerhub_repository = steps.sh (
+                script: ''' wget -q --user $USERNAME --password $PASSWORD https://registry.hub.docker.com/v1/repositories/odoopartners/odoo/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' ''',
+                returnStdout: true
+                ).replaceAll('\n', ', ')
+    }
 
-    return config.existing_tags_github_repository
+
+    return config.existing_tags_github_repository, config.existing_tags_dockerhub_repository
 
 
   }

5fba12f6c37843aa2609a0c60af831271d504016
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-01-11 18:34:02 -0500

    testing13 class library

--- src/org/ganemo/BuildImages.groovy
+++ src/org/ganemo/BuildImages.groovy
@@ -8,14 +8,11 @@ class BuildImages implements Serializable {
     this.steps = steps
   }
   
-  def mvn(args) {
-    steps.echo "${args}"
-  }
-  
   def validateTag(Map config = [:]) {
 
       def existing_tags_dockerhub_repository
       def existing_tags_github_repository
+
       steps.withCredentials([steps.gitUsernamePassword(credentialsId: "${config.credentials}",
                   gitToolName: 'git-tool')]) {
           steps.sh "git fetch --all --tags"
@@ -26,15 +23,47 @@ class BuildImages implements Serializable {
               returnStdout: true
               ).replaceAll('\n', ', ')
 
-          steps.withCredentials([steps.usernamePassword(credentialsId: "${config.registryCredential}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-              existing_tags_dockerhub_repository = steps.sh (
-                      script: ''' wget -q --user $USERNAME --password $PASSWORD https://registry.hub.docker.com/v1/repositories/odoopartners/odoo/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' ''',
-                      returnStdout: true
-                      ).replaceAll('\n', ', ')
-          }
+      steps.withCredentials([steps.usernamePassword(credentialsId: "${config.registryCredential}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+          existing_tags_dockerhub_repository = steps.sh (
+                  script: ''' wget -q --user $USERNAME --password $PASSWORD https://registry.hub.docker.com/v1/repositories/odoopartners/odoo/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' ''',
+                  returnStdout: true
+                  ).replaceAll('\n', ', ')
+      }
+
+      List<String> list_existing_tags_github = Arrays.asList(existing_tags_github_repository.split("\\s*,\\s*"))
+
+      List<String> list_existing_tags_docker = Arrays.asList(existing_tags_dockerhub_repository.split("\\s*,\\s*"))
 
 
-      return [existing_tags_github_repository, existing_tags_dockerhub_repository]
+      def exists = fileExists "${config.file_name}"
+      current_version = readFile "${config.file_name}"
+
+
+      if (exists) {
+          echo 'VERSION --> '+current_version
+      } else {
+          echo 'The file version does not exist'
+      }
+
+
+      tagname = registry+"-$current_version"                     
+      tagname_sanitized = tagname.trim()
+
+      tagname_for_github = "${config.BRANCH_NAME}-$current_version"
+
+      if (tagname_for_github.trim() in list_existing_tags_github) {
+          error("Build failed because of the tagname for GitHub already exists in the repository")
+      } else {
+          echo 'New Tag for Github Repository ---> '+ tagname_for_github
+      }
+
+      if (tagname_for_github.trim() in list_existing_tags_docker) {
+          error("Build failed because of the tagname for DockerHub already exists in the repository")
+      } else {
+          echo 'New Tag for DockerHub Repository ---> '+ tagname_for_github
+      }
+
+      return [existing_tags_github_repository, existing_tags_dockerhub_repository, tagname_sanitized, tagname, tagname_for_github]
 
 
   }

ea0f8fbc32d5acbad996e03bee1cdab7757631cb
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-01-31 17:01:39 -0500

    added new way for dockerhub repositories

--- src/org/ganemo/BuildImages.groovy
+++ src/org/ganemo/BuildImages.groovy
@@ -27,7 +27,7 @@ class BuildImages implements Serializable {
 
         steps.withCredentials([steps.usernamePassword(credentialsId: "${config.registryCredential}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             existing_tags_dockerhub_repository = steps.sh (
-                    script: ''' wget -q --user $USERNAME --password $PASSWORD https://registry.hub.docker.com/v1/repositories/odoopartners/odoo/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' ''',
+                    script: ''' wget -q --user $USERNAME --password $PASSWORD https://registry.hub.docker.com/v1/repositories/odoopartners/"${config.repo_name}"/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' ''',
                     returnStdout: true
                     ).replaceAll('\n', ', ')
         }
@@ -150,7 +150,7 @@ class BuildImages implements Serializable {
 
         steps.sshagent(credentials: ["${config.k8s_credentials}"]) {
             steps.sh """ 
-                ssh -o StrictHostKeyChecking=no -l ubuntu "${config.ip_from_master_node}" -A "kubectl -n odoo set image deployment/${config.BRANCH_NAME} odoo-${basename}=odoopartners/odoo:${config.tagname_for_github}" 
+                ssh -o StrictHostKeyChecking=no -l ubuntu "${config.ip_from_master_node}" -A "kubectl -n odoo set image deployment/${config.BRANCH_NAME} odoo-${basename}=odoopartners/${config.repo_name}:${config.tagname_for_github}" 
                 """
         }
 

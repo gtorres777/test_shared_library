2357ac490cbbcd07278ce6b36a9e55aa7498e217
gtorres777 <51068028+gtorres777@users.noreply.github.com>
2022-03-19 20:39:36 -0500

    Update variables.tf

--- resources/templates/terraform/variables.tf
+++ resources/templates/terraform/variables.tf
@@ -1,3 +1,16 @@
 variable "github_token" {
   description = "Github token to access repo."
 }
+
+variable "customer_name" {
+  description = "Customer deployment name."
+}
+
+variable "odoo_version" {
+  description = "Odoo version for the customer."
+}
+
+variable "test_branch" {
+  description = "Name for the test branch in the github repository."
+}
+

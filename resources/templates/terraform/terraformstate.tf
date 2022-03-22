eed380e7363a025b5dd96bb0f4d78d6a39d2da09
gtorres777 <51068028+gtorres777@users.noreply.github.com>
2022-03-21 19:20:50 -0500

    Update terraformstate.tf

--- resources/templates/terraform/terraformstate.tf
+++ resources/templates/terraform/terraformstate.tf
@@ -15,10 +15,6 @@ data "github_repository" "customer_name" {
     full_name = "gtorres777/${var.customer_name}"
 }
 
-output "gitname" {
-  value = data.github_repository.customer_name
-}
-
 resource "github_repository_file" "terraform_state" {
   repository          = data.github_repository.customer_name.name
   branch              = var.odoo_version
